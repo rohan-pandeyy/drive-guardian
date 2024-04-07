814089a1971bfb094bcd43b9b9e161268865ebf3
Sneha Choudhary <snehaxchoudharyx05@gmail.com>
2024-04-07 13:21:45 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from flask import Flask, request, Response
-import cv2
+from flask import Flask, request, Response, render_template
 from video_gen import process_video_realtime
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route('/')
-def root():
-    return app.send_static_file('./index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/video_stream')
 def video_stream():
     filename = request.args.get("filename")
     type = request.args.get("type")
     print(filename,type)
-    if type == 'v':
-        return Response(process_video_realtime(filename),
+    if type == 'w':
+        filename = 0
+    return Response(process_video_realtime(filename),
             mimetype='multipart/x-mixed-replace; boundary=frame')
-    elif type == 'w':
-        return Response(process_video_realtime(0),
-            mimetype='multipart/x-mixed-replace; boundary=frame')
+        
 
 if __name__ == '_main_':
     app.run(host='0.0.0.0', port=5000)  # Adjust host and port as needed
